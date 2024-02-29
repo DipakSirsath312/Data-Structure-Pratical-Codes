9be2f4cb9a72ad88167ffb0dfe753f8674955a3a
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-29 22:17:40 +0530

    [ADDED]:Dipak - Demonstrate Program Array And Print Array Element

---
+++ Array_Program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[5],i;
+	cout<<"Enter The Array Five Element:-"<<endl;
+	for(i=0;i<5;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<"Array Elements is:-"<<endl;
+	for(i=0;i<5;i++)
+	{
+		cout<<a[i]<<endl;
+	}
+}
